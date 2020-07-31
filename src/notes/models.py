aac4ebc4b8c62bd1982ee2135c1c66397d67fe8e
jinho6225 <jinho6225@hotmail.com>
2020-07-30 18:25:22 -0700

    create superuser, added models, registered admin

--- src/notes/models.py
+++ src/notes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+LABEL_CHOICES = (
+    ("P", "primary"),
+    ("SE", "secondary"),
+    ("S", "success"),
+    ("D", "danger"),
+    ("W", "Warninginfo"),
+    ("L", "light"),
+    ("D", "dark"),
+)
+
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.title
